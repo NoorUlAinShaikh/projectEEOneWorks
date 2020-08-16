46c4073f7c14bd62ea97695b8a2ad2c74880653e
Noorulain Shaikh <noorx.shaikh@gmail.com>
2020-08-17 01:56:49 +0530

    updates to package.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-if (process.env.NODE_ENV === PRODUCTION) {
+if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
